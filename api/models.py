e971a77dcc87e6d304cc58181cf03f71681b4cd2
nikhilem <nikhil.erkimutt@gmail.com>
2017-06-29 13:17:40 +0530

    removed background

--- api/models.py
+++ api/models.py
@@ -11,20 +11,11 @@ class Border(models.Model):
         return self.country_id
 
 
-def get_upload_path(instance, filename):
-    f_type = filename.split('.')[1]
-    print(f_type)
-    f_name = instance.name + '.' + f_type
-    print(f_name)
-    return 'images/stadiums/%s' % f_name
-
-
 class Stadium(models.Model):
     city = models.CharField(primary_key=True, max_length=30)
     name = models.CharField(unique=True, blank=False, null=False, max_length=30)
     latitude = models.FloatField()
     longitude = models.FloatField()
-    photo = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
 
     def __str__(self):
         return self.city
